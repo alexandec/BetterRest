6788ca1b6b15539fa36af554fcb4737504321e40
Chris Alexander <chris.alexander@adhocteam.us>
2024-05-28 16:26:02 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Christopher Michael Alexander on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
